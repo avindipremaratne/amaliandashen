b60cbb5ea4adeca918d7caccaeab6ca30d771156
Wix Remote Machine <support@wix.com>
2026-03-24 00:57:59 +0000

    Manual code changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,12 +15,11 @@ export default function Header() {
           {/* Logo/Image Box */}
           <div className="hidden md:flex items-center">
             <div className="relative h-12 w-32 bg-secondary/50 rounded-lg border border-border overflow-hidden flex items-center justify-center">
-              <Image 
-                src={logoUrl} 
-                alt="Event logo" 
+              <Image
+                src={logoUrl}
+                alt="Event logo"
                 className="w-full h-full object-cover"
                 width={128}
-                replaceable
               />
             </div>
           </div>
