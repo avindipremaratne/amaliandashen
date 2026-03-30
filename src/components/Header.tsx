1afe010a19531a1b9e06d4fc89d8bcd0e0399cc4
Wix Remote Machine <support@wix.com>
2026-03-30 02:21:42 +0000

    Manual code changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
+
   };
 
   return (
@@ -108,4 +108,3 @@ export default function Header() {
       </div>
     </header>
   );
-}
