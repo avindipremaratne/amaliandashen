27352aab5db7f93e11cce8127854706668600482
Wix Remote Machine <support@wix.com>
2026-06-05 05:07:32 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -112,6 +112,7 @@ export default function HomePage() {
         className="fixed inset-0 pointer-events-none z-0"
         style={{
           backgroundImage: 'url(https://static.wixstatic.com/media/b5e630_cd2aed73ea0c404f826bc9a96e467619~mv2.jpg))',
+          backgroundAttachment: 'fixed',
           backgroundPosition: 'top right',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover'
