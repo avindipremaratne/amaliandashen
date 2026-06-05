4c15a947028397efb2eeec9de4c941dd2ad70d95
Wix Remote Machine <support@wix.com>
2026-06-05 05:11:41 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -111,7 +111,7 @@ export default function HomePage() {
       <div
         className="fixed inset-0 pointer-events-none z-0"
         style={{
-          backgroundImage: 'url(https://static.wixstatic.com/media/b5e630_cd2aed73ea0c404f826bc9a96e467619~mv2.jpg))',
+          backgroundImage: 'url(https://static.wixstatic.com/media/b5e630_cd2aed73ea0c404f826bc9a96e467619~mv2.jpg)',
           backgroundAttachment: 'fixed',
           backgroundPosition: 'top right',
           backgroundRepeat: 'no-repeat',
