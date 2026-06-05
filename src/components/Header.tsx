7e4b28382ca73a758ab85bece46d7d6d2c248b4f
Wix Remote Machine <support@wix.com>
2026-06-05 05:26:30 +0000

    Manual code changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,6 @@
           src="https://static.wixstatic.com/media/b5e630_3c43452be4184c6c8a4adc35c634aa03~mv2.png"
           width={70}
           height={70}
-          sizes="(max-width: 768px) 64px, 80px"
           className="rounded-lg"
         />
       </div>
