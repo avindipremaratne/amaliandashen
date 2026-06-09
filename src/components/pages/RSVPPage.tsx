900a628506d8a3c10b605cfa2ea1299ab1dbb25d
Wix Remote Machine <support@wix.com>
2026-06-09 04:54:49 +0000

    Manual code changes

--- src/components/pages/RSVPPage.tsx
+++ src/components/pages/RSVPPage.tsx
@@ -174,7 +174,7 @@ export default function RSVPPage() {
       </section>
 
       {/* RSVP Form Section */}
-      <section className="py-8" style={{ backgroundColor: 'transparent' }}>
+      <section className="py-8">
         <div className="container mx-auto px-4">
           <div className="max-w-2xl mx-auto">
             {isSubmitted ? (
