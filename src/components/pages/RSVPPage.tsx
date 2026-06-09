6d5b2bc16884d69f6ff6accfd598d3d11b2d3f88
Wix Remote Machine <support@wix.com>
2026-06-09 05:13:56 +0000

    Manual code changes

--- src/components/pages/RSVPPage.tsx
+++ src/components/pages/RSVPPage.tsx
@@ -107,10 +107,10 @@ export default function RSVPPage() {
       }}
     >
       {/* Warm ivory overlay over entire page */}
-      <div
+      {/* <div
         className="absolute inset-0 pointer-events-none"
         style={{ backgroundColor: 'rgba(247, 243, 238, 0.55)', zIndex: 0 }}
-      />
+      /> */}
 
       <div className="relative z-10">
         <Header />
