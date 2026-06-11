74d999aed9fbc44d0d86278a723f5aaad7c148e5
Wix Remote Machine <support@wix.com>
2026-06-11 22:26:48 +0000

    Manual code changes

--- src/components/pages/RSVPPage.tsx
+++ src/components/pages/RSVPPage.tsx
@@ -129,7 +129,7 @@ export default function RSVPPage() {
         <Header />
 
         {/* Hero Section */}
-        <section className="py-16 md:py-24 pt-24 md:pt-32">
+        <section className="py-16 md:py-26 pt-24 md:pt-32">
           <div className="container mx-auto px-4 text-center">
             <AnimatedElement>
 
