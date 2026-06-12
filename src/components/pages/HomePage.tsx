bd739c8cf12e04d5eff5bc56e05efbaa95d574cc
Wix Remote Machine <support@wix.com>
2026-06-12 00:02:37 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -243,7 +243,9 @@ export default function HomePage() {
             lineHeight: 2,
           }}
         >
-          Mr. &amp; Mrs. Fernando <br/> &amp; Mr. &amp; Mrs. Perera
+          Mr. &amp; Mrs. Fernando <br/>
+          &amp; <br/>
+          Mr. &amp; Mrs. Perera
         </p>
         <p
           className="mb-8"
