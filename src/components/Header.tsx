597b724bc776edda91f3d5d41cd7598369c5c450
Wix Remote Machine <support@wix.com>
2026-06-16 23:09:56 +0000

    Manual code changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -86,7 +86,7 @@ export default function Header() {
                 src="https://static.wixstatic.com/media/b5e630_3c43452be4184c6c8a4adc35c634aa03~mv2.png"
                 width={70}
                 height={70}
-                sizes="(max-width: 768px) 64px, 80px"
+                sizes="(max-width: 768px) 40px, 70px"
                 className="rounded-lg"
               />
             </div>
