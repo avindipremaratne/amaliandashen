6bc902a227ca408ef0091277134f81f147b180b9
Wix Remote Machine <support@wix.com>
2026-06-17 03:42:48 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -10,6 +10,7 @@ import { motion } from 'framer-motion';
 import { Loader2 } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { RSVPs } from '@/entities';
 
 const FadeIn = ({
   children,
