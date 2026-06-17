3d7ac430f2d20dc2943f810d29ac96450f5e1e76
Wix Remote Machine <support@wix.com>
2026-06-17 03:18:26 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -307,7 +307,7 @@ export default function HomePage() {
                     <span
                       style={{
                         fontFamily: 'Montserrat, sans-serif',
-                        fontSize: '0.58rem',
+                        fontSize: '0.68rem',
                         fontWeight: 600,
                         letterSpacing: '0.3em',
                         textTransform: 'uppercase',
