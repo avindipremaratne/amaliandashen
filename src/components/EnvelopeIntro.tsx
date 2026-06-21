ba2f7ad88144fcf42997399df8e28aa6ca065371
Wix Remote Machine <support@wix.com>
2026-06-21 04:49:27 +0000

    Manual code changes

--- src/components/EnvelopeIntro.tsx
+++ src/components/EnvelopeIntro.tsx
@@ -98,7 +98,7 @@ export default function EnvelopeIntro({ onComplete }: EnvelopeIntroProps) {
             ref={isTablet ? videoRef : undefined}
             playsInline
             onEnded={handleEnded}
-            class
+            className='tablet'
             style={{
               position: 'absolute',
               inset: 0,
