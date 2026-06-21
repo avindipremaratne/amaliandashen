683066e3eb475e8776f322f4c737c54aa6426eb9
Wix Remote Machine <support@wix.com>
2026-06-21 02:03:37 +0000

    Manual code changes

--- src/components/EnvelopeIntro.tsx
+++ src/components/EnvelopeIntro.tsx
@@ -48,7 +48,7 @@ export default function EnvelopeIntro({ onComplete }: EnvelopeIntroProps) {
             onEnded={handleEnded}
             style={{
               position: 'fixed',
-              top: '50%',
+              top: '40%',
               left: '50%',
               transform: 'translate(-50%, -50%)',
               //minWidth: '100%',
