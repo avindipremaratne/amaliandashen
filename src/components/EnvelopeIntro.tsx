7701879b1770c13667a50af53f6d376181cc843d
Wix Remote Machine <support@wix.com>
2026-07-11 01:31:20 +0000

    Manual code changes

--- src/components/EnvelopeIntro.tsx
+++ src/components/EnvelopeIntro.tsx
@@ -329,7 +329,7 @@ export default function EnvelopeIntro({ onComplete }: EnvelopeIntroProps) {
               transition={{ delay: 1, duration: 0.8 }}
               style={{
                 position: 'absolute',
-                bottom: '60px',
+                bottom: '80px',
                 left: '0',
                 right: '0',
                 display: 'flex',
