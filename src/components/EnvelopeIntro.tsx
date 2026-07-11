6e8e65cdfc82a0ff470be0a5016b35765775b55c
Wix Remote Machine <support@wix.com>
2026-07-11 03:18:44 +0000

    Manual code changes

--- src/components/EnvelopeIntro.tsx
+++ src/components/EnvelopeIntro.tsx
@@ -330,7 +330,7 @@ export default function EnvelopeIntro({ onComplete }: EnvelopeIntroProps) {
     .tap-hint {
       top: 70% !important;
       bottom: auto !important;
-      left: 50% !important;
+      left: 49% !important;
       right: auto !important;
       transform: translate(-50%, -50%) !important;
     }
