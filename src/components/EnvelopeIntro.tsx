188c7710e30066e7bd704404e836cf5c3142ef60
Wix Remote Machine <support@wix.com>
2026-07-11 03:18:29 +0000

    Manual code changes

--- src/components/EnvelopeIntro.tsx
+++ src/components/EnvelopeIntro.tsx
@@ -328,7 +328,7 @@ export default function EnvelopeIntro({ onComplete }: EnvelopeIntroProps) {
 
     /* Center "Tap to Open" on the wax seal instead of near the bottom edge */
     .tap-hint {
-      top: 60% !important;
+      top: 70% !important;
       bottom: auto !important;
       left: 50% !important;
       right: auto !important;
